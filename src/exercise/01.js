ce7b7a8c013e542a6e2ddf8cf72a5242514cc22a
Rakib544 <md.rakib10122003@gmail.com>
2022-02-09 20:15:48 +0600

    learing about useReducer hook

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,21 +3,35 @@
 
 import * as React from 'react'
 
+// function countReducer(state, action) {
+//   return {
+//     ...state,
+//     // enabled action to take both function or object as arguments
+//     ...(typeof action === 'function' ? action(state) : action),
+//   }
+// }
+
 function countReducer(state, action) {
-  return {
-    ...state,
-    // enabled action to take both function or object as arguments
-    ...(typeof action === 'function' ? action(state) : action),
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
+
+  const increment = () => dispatch({type: 'INCREMENT', step})
+
   return <button onClick={increment}>{count}</button>
 }
 
@@ -26,3 +40,5 @@ function App() {
 }
 
 export default App
+
+// lesson 82 completed
